d03e8d3cf26071eefb9060f4e91c5a03492415f2
Noujox <darkosa15@gmai.com>
2026-09-15 10:16:34 -0300

    fix: enable automatic PWA updates

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === "development" && componentTagger(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       includeAssets: ['favicon.ico'],
       manifest: {
         name: 'Lector Bíblico - Estudio con Comentarios',
